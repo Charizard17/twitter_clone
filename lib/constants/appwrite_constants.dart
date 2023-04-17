f3c496118202a77fa6f780414c7d2af91cbe3cc8
Charizard17 <3723esad@gmail.com>
2023-04-17 13:36:42 +0200

    constants and theme files created. svg assets added

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppWriteConstants {
+  static const String databaseId = '643d2b0fbdfe980d0f0b';
+  static const String projectId = '643d27637b440472bb1a';
+  static const String endPoint = 'http://localhost:80/v1';
+}
